6d5dcedc7d32ad2343170db44bb0e12249b951b0
mauea22 <mauea22@hotmail.com>
2022-06-24 11:38:40 -0300

    Pipes Numeros

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent  {
+  ventasNetas: number = 2567789.5567;
+  porcentaje: number = 0.4856;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
