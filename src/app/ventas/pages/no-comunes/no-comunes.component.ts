8a2eb52a4b78b0e5b168d887b12cdba4611d575e
mauea22 <mauea22@hotmail.com>
2022-06-28 09:06:46 -0300

    Pipe i18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class NoComunesComponent implements OnInit {
-
+  //i18nselect
   nombre: string = 'Clara';
   genero: string = 'femenino';
 
   invitacionMapa = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  }
+
+  //i18nplural
+  clientes: string[] = ['Maria', 'Juan', 'Ana'];
+  clientesMapa = {
+    '=0': 'no tenemos clientes esperando',
+    '=1': 'tenemos 1 cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando'
   }
 
 
